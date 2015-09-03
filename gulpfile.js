f662ced2100c33363e1444e387217af749d9feaf
Daniel Leite de Oliveira <dleitee@gmail.com>
2015-09-03 15:58:31 -0300

    Correção watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,22 @@
 var gulp    = require('gulp');
 var uglify  = require('gulp-uglify');
 
+
+/*
+        Observa os arquivos js e executa a tarefa caso haja alteração.
+*/
+gulp.watch('src/*.js', ['minify']);
+
 /*
     Tarefa para minificar arquivos javascript
 */
 gulp.task('minify', function() {
-    
-    var glob = 'src/*.js';
-
-    /*
-        Observa os arquivos js e executa a tarefa caso haja alteração.
-    */
-    gulp.watch(glob, ['minify']);
 
     /*
         Seleciona todos os arquivos javascript na pasta "src", 
         minifica com o plugin uglify e envia o resultado para a pasta "dist"
     */
-    return gulp.src(glob)
+    return gulp.src('src/*.js')
                 .pipe(uglify())
                 .pipe(gulp.dest('dist'));
 
